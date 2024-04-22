cc4bd915f434ed971b5bf13018c5695403968961
Dilan Dev <dilan.developer22@gmail.com>
2024-04-22 17:55:10 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let $btnRandomColor = document.getElementById("btn__generate__color");
+let $title = document.getElementById("color__hexadecimal");
+let $body = document.body;
+
+$btnRandomColor.addEventListener("click", function () {
+  let longitud = 7;
+  let letras = "abcdef";
+  let numeros = "0123456789";
+
+  function generadarColorRandom(longitudColor, letra, numero) {
+    let allChareters = "";
+    let color = "";
+
+    allChareters += letra;
+    allChareters += numero;
+
+    for (let i = 0; i <= longitudColor; i++) {
+      let randomIndex = Math.floor(Math.random() * allChareters.length);
+      color += allChareters[randomIndex];
+    }
+    $title.innerHTML = ` #${color}`;
+    $body.style.background = `#${color}`;
+  }
+  return generadarColorRandom(longitud, letras, numeros);
+});
